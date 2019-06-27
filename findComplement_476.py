8af3ecf15537361fb84a60af88587d96a5b2beff
han.lu <han.lu@chinacache.com>
2019-06-27 14:44:44 +0800

    add findComplement

---
+++ findComplement_476.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+############################
+#File Name: findComplement_476.py
+#Author: luhan 
+#Created Time: 2019-06-25 10:44:26
+############################
+
+
+"""
+Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+Note:
+
+    The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+    You could assume no leading zero bit in the integer’s binary representation.
+
+Example 1:
+
+Input: 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+
+Example 2:
+
+Input: 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+
+"""
+
+class Solution(object):
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        return 2 ** (len(bin(num)) - 2) - 1 - num
+
+
+s = Solution()
+num = 5
+print s.findComplement(num)
